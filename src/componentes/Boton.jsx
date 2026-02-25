4102df264193f2ab209551714d9b1bb41ee25fc6
Santiago N. Silva <nehuen.silva.1997@gmail.com>
2026-02-25 18:35:41 -0300

    fix boton

--- src/componentes/Boton.jsx
+++ src/componentes/Boton.jsx
@@ -1,8 +1,8 @@
 
 export default function Boton(props) {
   return (
-    <button className={props.clases ?? ""} onClick={props.clickHandler ?? ""}>
+    <button className={props.clases ?? ""} onClick={props.clickHandler ?? (() => false)}>
       {props.etiqueta ?? ""}
-    </button>
+    </button >
   )
 }
